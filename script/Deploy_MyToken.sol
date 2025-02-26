41eab482301c91c62d9caab270a899ac888f98f4
Abdi Hassan <abdihassan@Abdis-MacBook-Air.local>
2025-02-26 10:07:42 +0100

    wow

--- script/Deploy_MyToken.sol
+++ script/Deploy_MyToken.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 import "forge-std/Script.sol";
 import "src/MyToken.sol";
 
-// forge script script/Deploy_MyToken.sol --broadcast -vvv --rpc-url https://eth-holesky.g.alchemy.com/v2/AwXNQB1sZceWq5qoYpeR7U0jsYYvoBHv --private-key 
+// forge script script/Deploy_MyToken.sol --broadcast -vvv --rpc-url https://eth-holesky.g.alchemy.com/v2/AwXNQB1sZceWq5qoYpeR7U0jsYYvoBHv --private-key
 //https://ethereum-holesky-rpc.publicnode.com
-// forge script script/Deploy_MyToken.sol --broadcast -vvv --rpc-url https://ethereum-holesky-rpc.publicnode.com --private-key 
+// forge script script/Deploy_MyToken.sol --broadcast -vvv --rpc-url https://ethereum-holesky-rpc.publicnode.com --private-key
 
-contract DeployTokenScript is Script{
-    function run() external{
+contract DeployTokenScript is Script {
+    function run() external {
         vm.startBroadcast();
 
         MyToken token = new MyToken();
